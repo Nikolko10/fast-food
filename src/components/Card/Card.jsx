8671324c2b72f5e5611562b88e33402d2e5b11eb
Nicky <kocakola2010@gmail.com>
2018-08-13 13:47:36 +0300

    Second commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Card.scss';
+
+class Card extends React.Component {
+	render() {
+		return <div className={styles.card}>sdvsdv</div>
+	}
+}
+
+export default Card;
